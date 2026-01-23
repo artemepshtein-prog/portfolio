39673e3752111019384ff2c93f28094a6625f95e
artemepshtein-prog <Artem.Epshtein@gmail.com>
2026-01-23 18:36:24 +0200

    this file is deleating idea

---
+++ translation.py
@@ -0,0 +1,24 @@
+translations = {
+    'cat': 'кот',
+    'dog': 'пёс',
+    'fish': 'рыба'
+}
+
+print("Введите слово на английском, которое хотите перевести")
+
+word = input()  # читаем ввод пользователя
+
+if word == "cat":
+    print(translations[word])
+if word == "dog":
+    print(translations[word])
+if word == "fish":
+    print(translations[word])
+else:
+    print("Извините, такого слова нет в словаре")
+
+# TODO:
+#  Написать программу, которая будет работать так
+#  > Введите слово на английском, которое хотите перевести
+#  < cat
+#  > Это слово переводится как "кот"
