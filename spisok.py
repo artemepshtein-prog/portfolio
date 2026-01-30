45ec3a84dbadc55c522b9d80c7ddb17445f3b58a
artemepshtein-prog <Artem.Epshtein@gmail.com>
2026-01-30 19:41:51 +0200

    this file is deleating idea

--- spisok.py
+++ spisok.py
@@ -1,4 +1,12 @@
-GRADES = (10, 5, 2, 6, 3, 5, 7, 0)
 
-average = sum(GRADES) / len(GRADES)
-print(average)
+
+
+calculate_mean = [1, 3, 5, 4, 5, 5]
+calculate_mean_2 = [4, 4, 3]
+
+def calculate():
+    all_grades = calculate_mean + calculate_mean_2
+    mean = sum(all_grades) / len(all_grades)
+    print(mean)
+
+calculate()
